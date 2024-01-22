b0cb903bdf3350e2ae36b94cc76d24c916e6c6fc
Haseebm01 <haseebdr01@gmail.com>
2024-01-21 21:01:55 -0800

    Installed Depends

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,32 @@ import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
 
 const Login = () => {
     return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to Unichate!</h2>
+
+                <div
+                    className="login-button google"
+                >
+                    <GoogleOutlined /> Sign In with Google
+                </div>
+                <br /> <br />
+
+                <div
+                    className="login-button facebook"
+                >
+                    <FacebookOutlined /> Sign In with Facebook
+                </div>
+
+            </div>
+
+        </div>
 
     );
 }
 
 
 export default Login;
+
+// "start": "react-scripts --openssl-legacy-provider start",
+// "build": "react-scripts --openssl-legacy-provider build"
