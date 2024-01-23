fc59c8c68857f26c2d7d61d588c63efbb6560907
Haseebm01 <haseebdr01@gmail.com>
2024-01-22 19:48:20 -0800

    modified

---
+++ src/components/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { firebase } from "firebase/app";
+import "firebase/auth";
+
+// export const auth = firebase.initializeApp({
+    const app = initializeApp({
+        apiKey: "AIzaSyBr64lJg3glicfKOdamHNPHPCUgGW0Ys5c",
+        authDomain: "unichat-d0d1e.firebaseapp.com",
+        projectId: "unichat-d0d1e",
+        storageBucket: "unichat-d0d1e.appspot.com",
+        messagingSenderId: "317257263688",
+        appId: "1:317257263688:web:3b5f6fd56bf71573c5467d"
+  });
+//   .auth();
+    export const auth = getAuth()
+    const database = getDatabase();
+
+    export { database }
+
+
