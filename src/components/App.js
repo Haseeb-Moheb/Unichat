f74cb2c1d6a75645efe451d89219c2fea4e64824
Haseebm01 <haseebdr01@gmail.com>
2024-01-24 17:50:39 -0800

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,19 @@
 import React from "react"
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-
-// import { AuthProvider } from "../contexts/AuthContext"
-
-// import Chats from "./Chats"
+import { AuthProvider } from "../contexts/AuthContext"
+import Chats from "./Chats"
 import Login from "./Login"
 
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
       <Router>
-        {/* <AuthProvider> */}
+        <AuthProvider>
           <Routes>
-            {/* <Route path="/chats" component={Chats} /> */}
+            <Route path="/chats" component={Chats} />
             <Route path="/" component={Login} />
           </Routes>
-        {/* </AuthProvider> */}
+        </AuthProvider>
       </Router>
     </div>
   )
